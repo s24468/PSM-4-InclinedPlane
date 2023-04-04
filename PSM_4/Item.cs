72941ca2dd97320a781535ffce1eb40cc92d7b8e
s24468 <s24468@pjwstk.edu.pl>
2023-04-04 20:11:38 +0200

    Introduce Item class

---
+++ PSM_4/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PSM_4
+{
+    public class Item
+    {
+        public double Radius { get; private set; }
+        public double Mass { get; private set; }
+        public double I { get; set; }
+
+        public double X;
+        public double Y;
+
+        public double XRounded => Math.Round(X, 7);
+
+        public double YRounded => Math.Round(Y, 7);
+
+
+        public Item(double radius, double mass, double x, double y)
+        {
+            I = 0;
+            Radius = radius;
+            Mass = mass;
+            X = x;
+            Y = y;
+        }
+    }
+}
