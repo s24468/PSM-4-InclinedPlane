97357e6ad07b04cc26f4e22b9d9fae0ad843d3c3
s24468 <s24468@pjwstk.edu.pl>
2023-04-06 07:55:27 +0200

    Completed project, however without final look

--- PSM_4/Item.cs
+++ PSM_4/Item.cs
@@ -9,11 +9,8 @@ namespace PSM_4
         public double I { get; set; } // moment bezwładności
         public double V { get; set; } //prędkość obiektu
 
-        public double Theta { get; set; } // kąt obrotu
+        public double Beta { get; set; } // kąt obrotu
         public double A { get; set; } //przyspieszenie liniowe obiektu
-        
-        public double W { get; set; } //prędkość kątowa
-        public double E { get; set; } //przyspieszenie kątowe
 
 
         public double X; //wartość wpółrzędnej X
@@ -33,9 +30,33 @@ namespace PSM_4
             Y = y;
             A = 0;
             V = 0;
+            Beta = 0;
+            Angle = new Angle(x, y+1);
+        }
+
+        public Angle Angle;
+
+
+    }
+
+    public class Angle
+    {
+        public double W { get; set; } //prędkość kątowa
+        public double E { get; set; } //przyspieszenie kątowe
+
+        public double XAngle; //wartość wpółrzędnej X kata
+        public double YAngle; //wartość wpółrzędnej Y kata
+
+        public double XAngleRounded => Math.Round(XAngle, 7);
+
+        public double YAngleRounded => Math.Round(YAngle, 7);
+
+        public Angle(double xAngle, double yAngle)
+        {
+            XAngle = xAngle;
+            YAngle = yAngle;
             W = 0;
             E = 0;
-            Theta = 0;
         }
     }
 }
--- PSM_4/Program.cs
+++ PSM_4/Program.cs
@@ -9,83 +9,44 @@ class Program
 
     static void Main(string[] args)
     {
-        var ball = new Item(0.1, 1, 0, 20);
+        var kula = new Item(1, 1, 0, 20);
 
         double totalTime = 5.0; // Czas symulacji (s)
         double timeStep = 0.05; // Krok czasowy (s)
 
         // Moment bezwładności dla kuli 
-        ball.I = 0.4 * (ball.Mass * Math.Pow(ball.Radius, 2));
+        kula.I = 0.4 * (kula.Mass * Math.Pow(kula.Radius, 2));
 
         // Symulacja dla kuli
-        ball.A = G * Math.Sin(Alpha) /
-                 (1 + (ball.I / (ball.Mass * Math.Pow(ball.Radius, 2)))); // przyspieszenie liniowe
-        DoSimulation(totalTime, timeStep, ball, "ball");
-        Console.WriteLine();
-        // Symulacja dla sfery
-        var sphere = new Item(0.1, 1, 0, 20);
-
-        // Moment bezwładności dla kuli 
-        sphere.I = (2 / 3) * (sphere.Mass * Math.Pow(sphere.Radius, 2));
-
-        sphere.A = G * Math.Sin(Alpha) /
-                   (1 + (sphere.I / (sphere.Mass * Math.Pow(sphere.Radius, 2))));
-        DoSimulation(totalTime, timeStep, sphere, "sphere");
-
-        // //- - - - - - - - - - - - - doAngleRotationSimulation - - - - - - - - - - - - -
-        // Console.WriteLine("ZACZYNAMY");
-        // ball = new Item(0.1, 1, 0, 20);
-        // ball.I = 0.4 * (ball.Mass * Math.Pow(ball.Radius, 2));
+        kula.A = G * Math.Sin(Alpha) /
+                 (1 + (kula.I / (kula.Mass * Math.Pow(kula.Radius, 2)))); // przyspieszenie liniowe
+        DoSimulation(totalTime, timeStep, kula, "ball");
+        
+        
+        //- - - - - - - - - - - - - - SYMULACJA DLA SFERY - - - - - - - - - - - - - -
+        
+        // Console.WriteLine();
+        // // Symulacja dla sfery
+        // var sphere = new Item(2, 1, 0, 20);
         //
-        // // Symulacja dla kuli
-        // ball.A = G * Math.Sin(Alpha) /
-        //          (1 + (ball.I / (ball.Mass * Math.Pow(ball.Radius, 2)))); // przyspieszenie liniowe
-        // for (double time = 0; time < totalTime; time += timeStep)
-        // {
-        //     ball.V += ball.A * timeStep; //aktualizacja prędości liniowej
+        // // Moment bezwładności dla sfery 
+        // sphere.I = (2 / 3) * (sphere.Mass * Math.Pow(sphere.Radius, 2));
         //
-        //     double vx = ball.V * Math.Cos(Alpha);
-        //     double vy = -ball.V * Math.Sin(Alpha);
-        //     double ax = ball.A * Math.Cos(Alpha);
-        //     double ay = -ball.A * Math.Sin(Alpha);
-        //
-        //
-        //     ball.X = UpdatePosition(ball.X, vx, ax, timeStep);
-        //     ball.Y = UpdatePosition(ball.Y, vy, ay, timeStep);
-        //
-        //
-        //     UpdateAngularValues(ball, timeStep);
-        //     UpdateRotation(ball, timeStep);
-        //     if (ball.YRounded <= 0)
-        //     {
-        //         return;
-        //     }
-        //
-        //     Console.WriteLine($"Czas: {time:0.00} s, X: {ball.XRounded}, Y: {ball.YRounded}, " +
-        //                       $"Theta: {ball.Theta * 180 / Math.PI:0.00} deg");
-        // }
+        // sphere.A = G * Math.Sin(Alpha) /
+        //            (1 + (sphere.I / (sphere.Mass * Math.Pow(sphere.Radius, 2))));
+        // DoSimulation(totalTime, timeStep, sphere, "sphere");
+        
+        //- - - - - - - - - - - - - - - - - - - - - - - - - - - -
     }
 
-    private static void UpdateRotation(Item item, double timeStep)
-    {
-        item.Theta += item.W * timeStep; // θ = θ + ω * Δt
-    }
-
-    private static void UpdateAngularValues(Item item, double timeStep)
-    {
-        double angularAcceleration = item.Mass * G * Math.Sin(Alpha) / item.I; // ε = (m * g * sin(α)) / I
-        item.W += angularAcceleration * timeStep; // ω = ω + ε * Δt
-    }
-
-
-    private static void DoSimulation(double totalTime, double timeStep, Item ball, string name)
+    private static void DoSimulation(double maxTime, double timeStep, Item ball, string name)
     {
         var positionList = new List<Tuple<double, double>>();
+        var angelList = new List<Tuple<double, double>>();
 
         Console.WriteLine($"Symulacja dla {name}:");
 
-        Console.WriteLine("X" + "\t \t" + "Y");
-        for (double time = 0; time < totalTime; time += timeStep)
+        for (double time = 0; time < maxTime; time += timeStep)
         {
             ball.V += ball.A * timeStep; //aktualizacja prędości liniowej
 
@@ -98,25 +59,39 @@ class Program
             ball.X = UpdatePosition(ball.X, vx, ax, timeStep);
             ball.Y = UpdatePosition(ball.Y, vy, ay, timeStep);
 
-
-            //kat obrotu
-            UpdateAngularValues(ball, timeStep);
-            UpdateRotation(ball, timeStep);
+            ball.Beta += ball.Angle.W * timeStep;
+            ball.Angle.XAngle = ball.X + ball.Radius * Math.Sin(ball.Beta);
+            ball.Angle.YAngle = ball.Y + ball.Radius * Math.Cos(ball.Beta);
+            ball.Angle.E = ball.A / ball.Radius ; // Poprawione obliczenie przyspieszenia kątowego
+            ball.Angle.W += ball.Angle.E * timeStep;
+            
+            
+            
             if (ball.YRounded <= 0)
             {
                 break;
             }
-            positionList.Add(new Tuple<double, double>(ball.XRounded,ball.YRounded));
-            // Console.WriteLine(ball.XRounded + " " + ball.YRounded);
-            
+
+            positionList.Add(new Tuple<double, double>(ball.XRounded, ball.YRounded));
+            angelList.Add(new Tuple<double, double>(ball.Angle.XAngleRounded, ball.Angle.YAngleRounded));
         }
+
+        Console.WriteLine("X" + "\t \t" + "Y");
+
         foreach (var tuple in positionList)
         {
-            Console.WriteLine($"({tuple.Item1}, {tuple.Item2})");
+            Console.WriteLine($"{tuple.Item1} {tuple.Item2}");
         }
-        Console.WriteLine($"{name} uderzył podłogi");
 
-        
+        Console.WriteLine("XANGLE" + "\t \t" + "YANGLE");
+
+        foreach (var tuple in angelList)
+        {
+            Console.WriteLine($"{tuple.Item1} {tuple.Item2}");
+        }
+
+
+        Console.WriteLine($"{name} uderzył podłogi");
     }
 
     static double UpdatePosition(double position, double v, double a, double timeStep)
